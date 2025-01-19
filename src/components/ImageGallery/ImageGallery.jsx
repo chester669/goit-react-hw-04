3300387d25cec1da14ff244518ba309e98261fd3
chester669 <viktorostash44@gmail.com>
2025-01-19 20:27:57 +0200

    fix immage gallery

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -4,16 +4,17 @@ import styles from "./ImageGallery.module.css";
 
 const ImageGallery = ({ images, onImageClick }) => {
   return (
-    <div className={styles.imageGallery}>
+    <ul className={styles.imageGallery}>
       {images.map((image) => (
-        <ImageCard
-          key={image.id}
-          imageUrl={image.urls.small}
-          altText={image.alt_description || "Image"}
-          onClick={() => onImageClick(image)}
-        />
+        <li key={image.id} className={styles.imageGalleryItem}>
+          <ImageCard
+            imageUrl={image.urls.small}
+            altText={image.alt_description || "Image"}
+            onClick={() => onImageClick(image)}
+          />
+        </li>
       ))}
-    </div>
+    </ul>
   );
 };
 
